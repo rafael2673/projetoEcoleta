5cad640cca4d6b3af8cd88027dc1bfb5a8424251
Rafael Franco <rafaelribeirofranco4@gmail.com>
2020-07-30 18:13:56 -0300

    Adicionando validação de dados

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
+import { celebrate, Joi } from 'celebrate';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -13,7 +14,25 @@ const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', upload.single('image'),pointsController.create);
+routes.post(
+  '/points', 
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      longitude: Joi.number().required(),
+      latitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    }),
+  }, {
+    abortEarly: false,
+  }
+  ),
+  pointsController.create);
 
 routes.get('/points', pointsController.index);
 
